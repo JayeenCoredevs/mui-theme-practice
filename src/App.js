03b67c623fefc6c378012edbb8048a7e17f21360
JayeenCoredevs <jayeen@coredevs.ltd>
2022-04-02 20:57:25 +0600

    theme is chaning and color variables can be controlled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import TstButton from "./Components/Framework/TstButton";
+import TstHeading from "./Components/Framework/TstHeading";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { deepOrange, purple } from "@mui/material/colors";
+import { CssBaseline } from "@mui/material";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [mode, setMode] = useState('light');
+  const theme = createTheme({
+    palette: {
+      mode,
+      ...(mode === 'light' ?
+        {
+          primary: {
+            main: purple[500],
+          }
+        } :
+        {
+          primary: deepOrange,
+        }
+      )
+
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <TstButton variant='contained' onClick={() => setMode(mode => mode === 'light' ? 'dark' : 'light')} size='small'>Hello world</TstButton>
+        <TstHeading variant={"h6"}>
+          Hello I am jayeen.
+        </TstHeading>
+      </div>
+    </ThemeProvider>
   );
 }
 
